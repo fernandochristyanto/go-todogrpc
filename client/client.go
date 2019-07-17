bf2bf3ee98469a5159921283f427190342eaa17e
Fernando Christyanto <christyantofernando@gmail.com>
2019-07-17 18:01:03 +0700

    Add basic TLS credentials for server and client

--- client/client.go
+++ client/client.go
@@ -5,16 +5,27 @@ import (
 	pb "github.com/fernandochristyanto/todogrpc/proto/todo"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
+	"path/filepath"
 )
 
 const (
-	address = "localhost:8080"
+	address    = "localhost:8080"
+	certPath   = "creds/server.crt"
+	serverName = "localhost" // Common Name
 )
 
 func main() {
+	// Create TLS credentials
+	cert, _ := filepath.Abs(certPath)
+	creds, err := credentials.NewClientTLSFromFile(cert, serverName)
+	if err != nil {
+		log.Fatalf("Error creating TLS creds: %s", err)
+	}
+
 	// Setup connection to gRPC server
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
 	}
--- server/main.go
+++ server/main.go
@@ -4,13 +4,16 @@ import (
 	pb "github.com/fernandochristyanto/todogrpc/proto/todo"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 )
 
 const (
-	port = ":8080"
+	certPath = "../creds/server.crt"
+	keyPath  = "../creds/server.key"
+	port     = ":8080"
 )
 
 // Implements the TodoTransaction interface
@@ -41,14 +44,41 @@ func (todoTransaction *todoTransactionImpl) GetTodos(ctx context.Context, in *pb
 	return &todosResponse, nil
 }
 
+/**
+* Steps in creating the credentials (SSL/TLS Certificates with openssl)
+* Files needed to be generated:
+*  - server.key: a private RSA key to sign and authenticate the public key
+*  - server.pem/server.crt: self-signed X.509 public keys for distribution
+*  - rootca.crt: a certificate authority public key for signing .csr files
+*  - host.csr: a certificate signing request to access the CA
+*
+* ------------------------------------------------------------------------
+* Commands:
+*  - openssl genrsa -out server.key 2048
+* 	 2048 bit RSA key (stronger keys are available as well).
+*  - openssl req -new -x509 -sha256 -key server.key \
+			 -out server.crt -days 3650
+	 Generate the certificate, and will also prompt you for some questions about the location, organization, and contact of the certificate holder.
+*
+*  - openssl req -new -sha256 -key server.key -out server.csr
+*  - openssl x509 -req -sha256 -in server.csr -signkey server.key \
+             -out server.crt -days 3650
+*    Generate a certificate signing request (.csr)
+*/
+
 func main() {
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Fail to open connection: %v", err)
 	}
 
-	// Declare new gRPC service
-	grpcServer := grpc.NewServer()
+	// Create new TLS credentials
+	creds, err := credentials.NewServerTLSFromFile(certPath, keyPath)
+	if err != nil {
+		log.Fatalf("Could not load TLS keys: %s", err)
+	}
+	// Declare new gRPC server with TLS
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 
 	// Register transaction servers
 	pb.RegisterTodoTransactionServer(grpcServer, &todoTransactionImpl{})
